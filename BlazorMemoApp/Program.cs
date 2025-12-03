48739c82c60e3a0e9d4d7feb2c603000133006cd
Sigid Sumargo <sigid.sumargo@gmail.com>
2025-12-03 17:29:38 +0700

    summary report group

--- BlazorMemoApp/Program.cs
+++ BlazorMemoApp/Program.cs
@@ -15,6 +15,7 @@ builder.Services.AddRazorComponents()
 
 //builder.Services.AddSyncfusionBlazor();
 
+
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddScoped<IdentityUserAccessor>();
 builder.Services.AddScoped<IdentityRedirectManager>();

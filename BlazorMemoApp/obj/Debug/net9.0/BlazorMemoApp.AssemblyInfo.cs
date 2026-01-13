d646ba9457a49135bb95e7bbd7112bdb9120f55c
Sigid Sumargo <sigid.sumargo@gmail.com>
2026-01-13 17:12:52 +0700

    fix QtyBlncAssc

--- BlazorMemoApp/obj/Debug/net9.0/BlazorMemoApp.AssemblyInfo.cs
+++ BlazorMemoApp/obj/Debug/net9.0/BlazorMemoApp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorMemoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b9b6eba2663ac7d94ee0d0d1242366fb787dc8c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8781596b6ee3ce6ae36b5eb810f803e9198f1a4c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorMemoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorMemoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- BlazorMemoApp/obj/Release/net9.0/BlazorMemoApp.AssemblyInfo.cs
+++ BlazorMemoApp/obj/Release/net9.0/BlazorMemoApp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorMemoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0b5a7e7ed417237da28b03dbdf21f66d81232f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8781596b6ee3ce6ae36b5eb810f803e9198f1a4c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorMemoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorMemoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
